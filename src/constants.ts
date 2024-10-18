06ded1843f1e5f0d37c7e31398f6b7a2057bdb19
Ben Ryan <61870507+bryan-insight@users.noreply.github.com>
2024-10-18 11:00:49 +0100

    Make Jira regex more specific

--- src/constants.ts
+++ src/constants.ts
@@ -18,7 +18,7 @@ export const DEFAULT_BRANCH_PATTERNS: RegExp[] = [/^master$/, /^production$/, /^
 /**
  * Regex to match JIRA issue keys.
  */
-export const JIRA_REGEX_MATCHER = /\d+-(([A-Z0-9]{1,10})|[a-z0-9]{1,10})/g;
+export const JIRA_REGEX_MATCHER = /(GNA-[0-9]{3,})/g;
 
 /**
  * Default total maximum number of additions after which jira-lint will discourage the PR as it is
