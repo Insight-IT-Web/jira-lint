0ea03af97ce96bbdbf319d929621afea70d2bbcb
Ben Ryan <61870507+bryan-insight@users.noreply.github.com>
2025-01-17 13:44:43 +0000

    Add missing comma

--- src/main.ts
+++ src/main.ts
@@ -89,7 +89,7 @@ async function run(): Promise<void> {
 
     if (typeof mergeGroup !== 'undefined') {
       const {
-        base_ref: baseBranch
+        base_ref: baseBranch,
         head_commit: { message: commitMessage },
       } = pullRequest as PullRequestParams;
     }
