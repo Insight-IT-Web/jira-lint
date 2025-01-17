ff8c54da3d40cd29757d890f1d7795c9ff822755
Ben Ryan <61870507+bryan-insight@users.noreply.github.com>
2025-01-17 18:45:42 +0000

    Specify capturing group in Jira key regex

--- src/constants.ts
+++ src/constants.ts
@@ -18,7 +18,7 @@ export const DEFAULT_BRANCH_PATTERNS: RegExp[] = [/^master$/, /^production$/, /^
 /**
  * Regex to match JIRA issue keys.
  */
-export const JIRA_REGEX_MATCHER = /^(GNA|PRT-[0-9]{3,})/;
+export const JIRA_REGEX_MATCHER = /^((?:GNA|PRT)-[0-9]{3,})/;
 
 /**
  * Default total maximum number of additions after which jira-lint will discourage the PR as it is
