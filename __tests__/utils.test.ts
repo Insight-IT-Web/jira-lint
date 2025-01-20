e13dd7c8141a1bb2b937d338f95cf0be4364378f
Ben Ryan <61870507+bryan-insight@users.noreply.github.com>
2025-01-20 16:50:38 +0000

    Update utils.test.ts

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -7,6 +7,7 @@ import { JIRADetails } from '../src/types';
 
 jest.spyOn(console, 'log').mockImplementation(); // avoid actual console.log in test output
 
+describe('Jira issue key extractor', () => {
   it('gets jira key from different PR title names', () => {
     expect(getJIRAIssueKeys('GNA-1234 fix for something, GNA-34535 another fix')).toEqual(['GNA-1234']);
     expect(getJIRAIssueKeys('PRT-5678 - a platform fix')).toEqual(['PRT-5678']);
